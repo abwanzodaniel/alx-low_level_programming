5263b7344cd7202bb38677884e88b8c91904b966
abwanzodaniel <abwanzodaniel@gmail.com>
2022-10-18 15:15:18 -0700

    10 times alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet in lowercase 10 times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int count = 0;
+
+	while (count <= 9)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		_putchar('/n');
+	}
+}
